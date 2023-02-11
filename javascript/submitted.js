1b4feedd8054ca8e9a6d2e30745faaa9d132d67b
Baroon-Shrestha <119175371+Baroon-Shrestha@users.noreply.github.com>
2023-02-11 23:21:58 +0545

    Project

---
+++ javascript/submitted.js
@@ -0,0 +1,8 @@
+const answer = document.getElementById("results");
+answer.style.color = "white";
+answer.style.padding = "60px 0 0 275px"
+answer.style.margin = "0 0 20px 0"
+const resultsList = document.getElementById('results')
+        new URLSearchParams(window.location.search).forEach((value,name)=>{results.append(`${name}: ${value}`)
+        resultsList.append(document.createElement('br')) 
+        })
